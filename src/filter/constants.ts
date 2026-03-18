6e0134ee4f893d7ba6b90ac568993604b3d15a63
oleg.pronchenko <traeop@gmail.com>
2026-03-18 03:32:34 +0200

    refactor: remove unnecessary comments from constants in filter and sort modules

--- src/filter/constants.ts
+++ src/filter/constants.ts
@@ -1,6 +1,2 @@
-/**
- * Shared constants used by the filter module.
- */
-
 export const DEFER_FILTER_MUTATION_INDEX_UPDATES_KEY =
   "deferFilterMutationIndexUpdates";
--- src/sort/constants.ts
+++ src/sort/constants.ts
@@ -1,6 +1,2 @@
-/**
- * Shared constants used by the sort module.
- */
-
 export const DEFER_SORT_MUTATION_CACHE_UPDATES_KEY =
   "deferSortMutationCacheUpdates";
